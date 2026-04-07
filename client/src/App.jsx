44abd4da91e367624279082b434a723916d60267
Kalpna verma <vermakalpna714@gmail.com>
2026-04-07 10:46:20 +0530

    Update ServerUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewPage from "./pages/InterviewPage";
 import InterviewHistory from "./pages/InterviewHistory";
 
 
-export  const ServerUrl ="http://localhost:8000"
+export  const ServerUrl ="https://intellihire-ai-pa5t.onrender.com"
 function App() {
 
   const dispatch=useDispatch()
